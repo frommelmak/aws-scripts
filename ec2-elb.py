f6d88c3dbce4a1bb7c82763252ecc2cc96e322ff
frommelmak <mmartinez@admira.com>
2015-11-05 12:48:19 +0100

    printing instance name using the name tag

--- ec2-elb.py
+++ ec2-elb.py
@@ -7,11 +7,13 @@ import argparse
 def list_elb():
    client = boto3.client('elb')
    response = client.describe_load_balancers()
-   #response = client.describe_load_balancers(LoadBalancerNames=['apielb', ], PageSize=123)
+   ec2 = boto3.resource('ec2')
    for elb in range(len(response.get('LoadBalancerDescriptions'))):
       print response.get('LoadBalancerDescriptions')[elb].get('LoadBalancerName')
       for instance in range(len(response.get('LoadBalancerDescriptions')[elb].get('Instances'))):
-         print u"  └── %s" % response.get('LoadBalancerDescriptions')[elb].get('Instances')[instance].get('InstanceId')
+         id = response.get('LoadBalancerDescriptions')[elb].get('Instances')[instance].get('InstanceId')
+         instance = ec2.Instance(id)
+         print u"  └── %s (%s)" % (id, instance.tags[0]['Value'])
 
 
 def main():
