3fa01cd520093f76f06300eee92d591677d37ad4
frommelmak <mmartinez@admira.com>
2016-04-08 20:32:08 +0200

    vpc_id added to the output

--- ec2-instances.py
+++ ec2-instances.py
@@ -8,12 +8,12 @@ import paramiko
 def list_instances(Filter):
    ec2 = boto3.resource('ec2')
    instances = ec2.instances.filter(Filters=Filter)
-   columns_format="%-3s %-26s %-16s %-12s %-12s %-16s"
-   print columns_format % ("num", "Name", "Public IP", "ID", "Type", "Status")
+   columns_format="%-3s %-26s %-16s %-12s %-12s %-12s %-16s"
+   print columns_format % ("num", "Name", "Public IP", "ID", "Type", "VPC", "Status")
    num = 1
    hosts = {}
    for i in instances:
-      print columns_format % (num, i.tags[0]['Value'], i.public_ip_address, i.id, i.instance_type, i.state['Name'])
+      print columns_format % (num, i.tags[0]['Value'], i.public_ip_address, i.id, i.instance_type, i.vpc_id, i.state['Name'])
       num = num + 1
       hosts[i.tags[0]['Value']]=i.public_ip_address
    return hosts
