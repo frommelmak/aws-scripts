37934fa60d3c05910d25015d8c47c4d1467c8a24
Marcos Martinez <mmartinez@admira.com>
2016-06-27 15:57:25 +0200

    (work in progres) Date and continue lifecycle added

--- lifecycle-hook-worker.py
+++ lifecycle-hook-worker.py
@@ -5,12 +5,15 @@ import sys
 import argparse
 import ast
 import urllib2
+from subprocess import call
+import time
+from datetime import datetime
 
 def sqs_consumer(qname):
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName=qname)
     client = boto3.client('sqs')
-    message = client.receive_message(QueueUrl=queue.url, MaxNumberOfMessages=1, WaitTimeSeconds=10)
+    message = client.receive_message(QueueUrl=queue.url, MaxNumberOfMessages=1, WaitTimeSeconds=20)
     if message.get('Messages'):
        m = message.get('Messages')[0]
        body = ast.literal_eval(m['Body'])
@@ -26,16 +29,6 @@ def get_ec2instanceid():
     instanceid = response.read()
     return instanceid
 
-def complete_lifecycle(lhn,asgn,lat):
-    client = boto3.client('autoscaling')
-    response = client.complete_lifecycle_action(
-       LifecycleHookName=lhn,
-       AutoScalingGroupName=asgn,
-       LifecycleActionToken=lat,
-       LivecycleActionResult='CONTINUE',
-       InstanceID=''
-    )
-
 def main():
     parser = argparse.ArgumentParser(description='SQS Lifecycle hook consumer and trigger')
     parser.add_argument('-q', '--queue', required=True,
@@ -48,25 +41,39 @@ def main():
                         help='Life Cycle Hook Name')
     parser.add_argument('-e', '--execute', required=True,
                         help="The filepath of the triggered script")
+    parser.add_argument('-w', '--wait', default=60, type=int,
+                        help="Time between query loops in seconds (default: 60)")
 
-    arg = parser.parse_args()  
+    arg = parser.parse_args()
 
     if arg.state == "LAUNCHING":
        state = "autoscaling:EC2_INSTANCE_TERMINATING"
     elif arg.state == "TERMINATING":
        state = "autoscaling:EC2_INSTANCE_LAUNCHING"
-   
+
     ec2instanceid = get_ec2instanceid()
-    
+    print ("%s Listening for %s SQS messages using long polling") % (datetime.now().strftime('%H:%M:%S %D'), ec2instanceid)
+
     while 1:
        sqs_msg = sqs_consumer(arg.queue)
        if sqs_msg['Event'] == "autoscaling:TEST_NOTIFICATION":
-          print ("Tests message consumed")
+          print ("%s Tests message consumed") % datetime.now().strftime('%H:%M:%S %D')
        elif sqs_msg['timeout']:
-          print ("There are no messages in the queue. Trying again")         
+          print ("%s There are no messages in the queue. Sleeping and trying again") % datetime.now().strftime('%H:%M:%S %D')
        elif (sqs_msg['Event'] == state) and (sqs_msg['EC2InstanceId'] == ec2instanceid):
-          print "%s hook message received" % arg.state
-          print "Calling to exec filepath"
+          print "%s %s hook message received" % (datetime.now().strftime('%H:%M:%S %D'), arg.state)
+          print "%s Executing filepath" % datetime.now().strftime('%H:%M:%S %D')
+          call(["uname", "-a"])
+          print "%s Completing lifecyle action" % datetime.now().strftime('%H:%M:%S %D')
+          as_client = boto3.client('autoscaling')
+          response = as_client.complete_lifecycle_action(
+             LifecycleHookName=arg.hookName,
+             AutoScalingGroupName=arg.group,
+             LifecycleActionToken=sqs_msg['LifecycleActionToken'],
+             LivecycleActionResult='CONTINUE',
+             InstanceID=ec2instanceid
+          )
+       time.sleep(arg.wait)
 
 if __name__ == '__main__':
     sys.exit(main())
