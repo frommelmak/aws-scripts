dfa427f9406be2f9b2f8fc70a06bef880406a089
frommelmak <frommelmak@gmail.com>
2017-03-20 16:10:08 +0100

    Private IP added to the default output

--- ec2-instances.py
+++ ec2-instances.py
@@ -8,12 +8,21 @@ import paramiko
 def list_instances(Filter):
    ec2 = boto3.resource('ec2')
    instances = ec2.instances.filter(Filters=Filter)
-   columns_format="%-3s %-26s %-16s %-20s %-12s %-12s %-16s"
-   print columns_format % ("num", "Name", "Public IP", "ID", "Type", "VPC", "Status")
+   columns_format="%-3s %-26s %-16s %-16s %-20s %-12s %-12s %-16s"
+   print columns_format % ("num", "Name", "Public IP", "Private IP", "ID", "Type", "VPC", "Status")
    num = 1
    hosts = [] 
    for i in instances:
-      print columns_format % (num, i.tags[0]['Value'], i.public_ip_address, i.id, i.instance_type, i.vpc_id, i.state['Name'])
+      print columns_format % (
+                               num,
+                               i.tags[0]['Value'],
+                               i.public_ip_address,
+                               i.private_ip_address,
+                               i.id,
+                               i.instance_type,
+                               i.vpc_id,
+                               i.state['Name']
+                             )
       num = num + 1
       item={'id': i.id, 'ip': i.public_ip_address, 'hostname': i.tags[0]['Value'],}
       hosts.append(item)
