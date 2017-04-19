2ac70a3b3a137367a90a4b95490c34c1af388277
frommelmak <frommelmak@gmail.com>
2017-04-19 17:13:39 +0200

    Consume the message from the queue only if the instance ID matches

--- lifecycle-hook-worker.py
+++ lifecycle-hook-worker.py
@@ -10,7 +10,8 @@ import time
 from datetime import datetime
 import shlex
 
-def sqs_consumer(qname):
+
+def sqs_get_msg(qname):
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName=qname)
     client = boto3.client('sqs')
@@ -19,20 +20,30 @@ def sqs_consumer(qname):
        m = message.get('Messages')[0]
        body = ast.literal_eval(m['Body'])
        receipt_handle = m['ReceiptHandle']
-       response = client.delete_message(QueueUrl=queue.url, ReceiptHandle=receipt_handle)
+       #response = client.delete_message(QueueUrl=queue.url, ReceiptHandle=receipt_handle)
     else:
-       body = {'timeout': True, 'Event': False}
-    return(body)
+       body = {'LifecycleTransition': False}
+       receipt_handle = ""
+    return body, receipt_handle
+
+
+def sqs_delete_msg(qname, receipt_handle):
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName=qname)
+    client = boto3.client('sqs')
+    response = client.delete_message(QueueUrl=queue.url, ReceiptHandle=receipt_handle)
+
 
 def get_ec2instanceid():
     # curl http://169.254.169.254/latest/meta-data/instance-id
     try:
        response = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id')
     except:
-       sys.exit("%s I am not running in EC2. Aborting!!" % datetime.now().strftime('%H:%M:%S %D')) 
-        
+       sys.exit("%s I am not running in EC2. Aborting!!" % datetime.now().strftime('%H:%M:%S %D'))
+
     instanceid = response.read()
     return instanceid
+
 
 def main():
     parser = argparse.ArgumentParser(description='SQS Lifecycle hook consumer and trigger')
@@ -52,23 +63,24 @@ def main():
     arg = parser.parse_args()
 
     if arg.state == "LAUNCHING":
-       state = "autoscaling:EC2_INSTANCE_TERMINATING"
-    elif arg.state == "TERMINATING":
        state = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    elif arg.state == "TERMINATING":
+       state = "autoscaling:EC2_INSTANCE_TERMINATING"
 
     cmd_args = shlex.split(arg.execute)
-    
-    print ("%s Getting EC2 instance ID") % datetime.now().strftime('%H:%M:%S %D')  
+
+    print ("%s Getting EC2 instance ID") % datetime.now().strftime('%H:%M:%S %D')
     ec2instanceid = get_ec2instanceid()
     print ("%s Listening for %s SQS messages using long polling") % (datetime.now().strftime('%H:%M:%S %D'), ec2instanceid)
 
     while 1:
-       sqs_msg = sqs_consumer(arg.queue)
-       if sqs_msg['Event'] == "autoscaling:TEST_NOTIFICATION":
+       sqs_msg, sqs_receipt_handle = sqs_get_msg(arg.queue)
+       if sqs_msg['LifecycleTransition'] == "autoscaling:TEST_NOTIFICATION":
           print ("%s Tests message consumed") % datetime.now().strftime('%H:%M:%S %D')
-       elif sqs_msg['timeout']:
+       elif sqs_msg['LifecycleTransition'] == False:
           print ("%s There are no messages in the queue. Sleeping and trying again") % datetime.now().strftime('%H:%M:%S %D')
-       elif (sqs_msg['Event'] == state) and (sqs_msg['EC2InstanceId'] == ec2instanceid):
+       elif (sqs_msg['LifecycleTransition'] == state) and (sqs_msg['EC2InstanceId'] == ec2instanceid):
+          sqs_delete_msq(arg.queue, sqs_receipt_handle)
           print "%s %s hook message received" % (datetime.now().strftime('%H:%M:%S %D'), arg.state)
           print "%s Executing filepath" % datetime.now().strftime('%H:%M:%S %D')
           call(cmd_args)
