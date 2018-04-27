6ab16c678ab1748ec04b74e095f662a465028c27
John Doe <mmartinez@admira.com>
2018-04-27 10:51:18 +0200

    The tag key showed didn't belong to the instance ID

--- ec2-elb.py
+++ ec2-elb.py
@@ -4,20 +4,22 @@ import boto3
 import sys
 import argparse
 
-def list_elb():
-   client = boto3.client('elb')
-   response = client.describe_load_balancers()
-   ec2 = boto3.resource('ec2')
-   for elb in range(len(response.get('LoadBalancerDescriptions'))):
+def get_tag(ec2, id):
+   instance = ec2.Instance(id)
+   return instance.tags[0]['Value']
+
+def list_elb(ec2):
+    client = boto3.client('elb')
+    response = client.describe_load_balancers()
+    for elb in range(len(response.get('LoadBalancerDescriptions'))):
       load_balancer_name = response.get('LoadBalancerDescriptions')[elb].get('LoadBalancerName')
+      instance_state = client.describe_instance_health(LoadBalancerName=load_balancer_name)
       print u"%s" % load_balancer_name
       for instance in range(len(response.get('LoadBalancerDescriptions')[elb].get('Instances'))):
-         id = response.get('LoadBalancerDescriptions')[elb].get('Instances')[instance].get('InstanceId')
-         instance_id = ec2.Instance(id)
-         instance_state = client.describe_instance_health(LoadBalancerName=load_balancer_name)
+         instance_id = instance_state.get('InstanceStates')[instance].get('InstanceId')
          print u"  └── %-20s (%s) Status: %s, Description: %s" % (
-                                                               id, 
-                                                               instance_id.tags[0]['Value'],
+                                                               instance_id,
+                                                               get_tag(ec2, instance_id),
                                                                instance_state.get('InstanceStates')[instance].get('State'),
                                                                instance_state.get('InstanceStates')[instance].get('Description')
                                                               )
@@ -25,9 +27,10 @@ def list_elb():
 
 def main():
     parser = argparse.ArgumentParser(description='For every Elastic Load Balancer list the attached instances')
-
     arg = parser.parse_args()
-    list_elb()
+
+    ec2 = boto3.resource('ec2')
+    list_elb(ec2)
 
 if __name__ == '__main__':
     sys.exit(main())
