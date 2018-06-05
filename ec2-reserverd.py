42698771750c89b3537b2dd442d3b52b6b00885a
Marcos Martinez <mmartinez@admira.com>
2018-06-05 08:31:00 +0200

    State and type filters added

--- ec2-reserverd.py
+++ ec2-reserverd.py
@@ -3,28 +3,46 @@
 import boto3
 import sys
 import argparse
+from datetime import datetime, timedelta
 
-def list_reserved_instances(ec2):
+def list_reserved_instances(ec2, filters):
     client = boto3.client('ec2')
-    filters = []
-    filters.append({'Name': 'state', 'Values': ['active']})
     response = client.describe_reserved_instances(Filters=filters)
     size = len(response.get('ReservedInstances'))
-    columns_format="%-5s %-36s %-10s %-6s"
-    print columns_format % ("num", "Reserved Id", "Instances", "Type")
+    columns_format="%-36s %-10s %-12s %-24s %-18s %-14s %-s %-6s"
+    print columns_format % ("Reserved Id", "Instances", "Type", "Product Description", "Scope", "Region", "Duration", "End")
     for n in range(size):
         id=response.get('ReservedInstances')[n].get('ReservedInstancesId')
         count=response.get('ReservedInstances')[n].get('InstanceCount')
         type=response.get('ReservedInstances')[n].get('InstanceType')
-        print columns_format % (n, id, count, type)
+        product=response.get('ReservedInstances')[n].get('ProductDescription')
+        scope=response.get('ReservedInstances')[n].get('Scope')
+        region=response.get('ReservedInstances')[n].get('AvailabilityZone')
+        duration=response.get('ReservedInstances')[n].get('Duration')
+        td=timedelta(seconds=int(duration))
+        end=response.get('ReservedInstances')[n].get('End')
+        print columns_format % (id, count, type, product, scope, region, td.days, end)
         
 def main():
     parser = argparse.ArgumentParser(description='Show active reserved EC2 instances')
+    parser.add_argument('-s', '--state', action='store',
+                        choices=['payment-pending', 'active', 'payment-failed', 'retired'],
+                        help="Filer result by reservation state.")
+    parser.add_argument('-t', '--type',
+                        help="Filer result by instance type.")
+
     arg = parser.parse_args()
 
+    filters=[]
+
+    if arg.state:
+        filters.append({'Name': 'state', 'Values': ["" + arg.state + ""]})
+
+    if arg.type:
+        filters.append({'Name': 'instance-type', 'Values': ["*" + arg.type + "*"]})
+
     ec2 = boto3.resource('ec2')
-    list_reserved_instances(ec2)
-    
+    list_reserved_instances(ec2, filters)
 
 if __name__ == '__main__':
     sys.exit(main())
