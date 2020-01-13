69be5e6584716fc09b48555c06f430c3ef2de238
frommelmak <frommelmak@gmail.com>
2020-01-13 15:27:14 +0100

    transform ec2-elb.py into Python3.x/2.7 valid code

--- aws-scripts/ec2-elb.py
+++ aws-scripts/ec2-elb.py
@@ -14,15 +14,15 @@ def list_elb(ec2):
     for elb in range(len(response.get('LoadBalancerDescriptions'))):
       load_balancer_name = response.get('LoadBalancerDescriptions')[elb].get('LoadBalancerName')
       instance_state = client.describe_instance_health(LoadBalancerName=load_balancer_name)
-      print u"%s" % load_balancer_name
+      print("%s" % load_balancer_name)
       for instance in range(len(response.get('LoadBalancerDescriptions')[elb].get('Instances'))):
          instance_id = instance_state.get('InstanceStates')[instance].get('InstanceId')
-         print u"  \_ %-20s (%s) Status: %s, Description: %s" % (
+         print("  \_ %-20s (%s) Status: %s, Description: %s" % (
                                                                instance_id,
                                                                get_tag(ec2, instance_id),
                                                                instance_state.get('InstanceStates')[instance].get('State'),
                                                                instance_state.get('InstanceStates')[instance].get('Description')
-                                                              )
+                                                              ))
 
 
 def main():
