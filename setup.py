627350fe6a1c6305e6fe992fbc30d6afe9e4b7e0
marcos-martinez <mmartinez@admira.com>
2021-05-28 10:25:13 +0200

    version updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "aws-scripts",
-    version = "0.0.21",
+    version = "0.0.22",
     author = "Marcos Martinez",
     author_email = "frommelmak@gmail.com",
     description = "Some useful AWS scripts I use from time to time",
