fb162e4e8f869992ec847f1fb2712ff1ac364d13
frommelmak <frommelmak@gmail.com>
2023-01-26 23:13:08 +0100

    group targets by availability zone (needs to improve poerformance but commited anyway)

--- aws-scripts/ec2-elb.py
+++ aws-scripts/ec2-elb.py
@@ -49,8 +49,6 @@ def list_elbv2(ec2, region):
       load_balancer_arn = response.get('LoadBalancers')[elb].get('LoadBalancerArn')
       load_balancer_type = response.get('LoadBalancers')[elb].get('Type')
       load_balancer_zones = []
-      for n in range(len(response.get('LoadBalancers')[elb].get('AvailabilityZones'))):
-          load_balancer_zones.append(response.get('LoadBalancers')[elb].get('AvailabilityZones')[n].get('ZoneName'))
       load_balancer_tg = client.describe_target_groups(LoadBalancerArn=load_balancer_arn)
       elb_branch = tree.add(":file_folder:[bold white] "+load_balancer_name+" Type: "+load_balancer_type)
       tg_folder = elb_branch.add(":file_folder: Target Groups")
@@ -58,27 +56,32 @@ def list_elbv2(ec2, region):
           tg_dict = load_balancer_tg.get('TargetGroups')[tg]
           tg_branch = tg_folder.add(":file_folder:[bold white] "+tg_dict.get('TargetGroupName')+" [not bold]Type: "+tg_dict.get('TargetType'))
           target_health = client.describe_target_health(TargetGroupArn=tg_dict.get('TargetGroupArn'))
-          for t in range(len(target_health.get('TargetHealthDescriptions'))):
-              target_id=target_health.get('TargetHealthDescriptions')[t].get('Target').get('Id')
-              target_state=target_health.get('TargetHealthDescriptions')[t].get('TargetHealth').get('State')
-              target_state_desc=target_health.get('TargetHealthDescriptions')[t].get('TargetHealth').get('Description')
-              # Target Type: instance'|'ip'|'lambda'|'alb'
-              if tg_dict.get('TargetType') == 'instance':
-                  target_name, zone =get_info(ec2, target_id)
-              elif tg_dict.get('TargetType') == 'ip':
-                  target_name="ip"
-              elif tg_dict.get('TargetType') == 'lambda':
-                  target_name="lambda"
-              elif tg_dict.get('TargetType') == 'alb':
-                  target_name="alb"
-              # TargetHealth State: initial'|'healthy'|'unhealthy'|'unused'|'draining'|'unavailable'
-              if target_state == 'healthy':
-                  target_state = "[green]"+target_state
-              elif target_state == 'unhealthy':
-                  target_state = "[red]"+target_state
-              else:
-                  target_state = "[orange1]"+target_state
-              tg_branch.add("[cyan]"+target_id+" [white]("+target_name+") [white]Status: "+str(target_state)+" [white]Description: "+str(target_state_desc))
+          for zone in range(len(response.get('LoadBalancers')[elb].get('AvailabilityZones'))):
+              zone_name =response.get('LoadBalancers')[elb].get('AvailabilityZones')[zone].get('ZoneName')
+              zone_tree = tg_branch.add(":file_folder: [bold white]"+zone_name)
+              for t in range(len(target_health.get('TargetHealthDescriptions'))):
+                  target_id=target_health.get('TargetHealthDescriptions')[t].get('Target').get('Id')
+                  elb_name, target_zone=get_info(ec2, target_id)
+                  target_state=target_health.get('TargetHealthDescriptions')[t].get('TargetHealth').get('State')
+                  target_state_desc=target_health.get('TargetHealthDescriptions')[t].get('TargetHealth').get('Description')
+                  # Target Type: instance'|'ip'|'lambda'|'alb'
+                  if tg_dict.get('TargetType') == 'instance':
+                      target_name, zone =get_info(ec2, target_id)
+                  elif tg_dict.get('TargetType') == 'ip':
+                      target_name="ip"
+                  elif tg_dict.get('TargetType') == 'lambda':
+                      target_name="lambda"
+                  elif tg_dict.get('TargetType') == 'alb':
+                      target_name="alb"
+                  # TargetHealth State: initial'|'healthy'|'unhealthy'|'unused'|'draining'|'unavailable'
+                  if target_state == 'healthy':
+                      target_state = "[green]"+target_state
+                  elif target_state == 'unhealthy':
+                      target_state = "[red]"+target_state
+                  else:
+                      target_state = "[orange1]"+target_state
+                  if target_zone == zone_name:
+                      zone_tree.add("[cyan]"+target_id+" [white]("+target_name+") [white]Status: "+str(target_state)+" [white]Description: "+str(target_state_desc))
     print(tree)
 
 def main():
